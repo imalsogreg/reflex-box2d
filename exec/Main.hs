185084f493af646ecda1bbc4a12980d279ee7ef2
Greg Hale <imalsogreg@gmail.com>
2016-10-31 14:21:24 -0400

    Refactor

--- exec/Main.hs
+++ exec/Main.hs
@@ -1,5 +1,6 @@
 {-# language OverloadedStrings #-}
 {-# language FlexibleContexts #-}
+{-# language RecursiveDo #-}
 
 module Main where
 
@@ -17,64 +18,85 @@ import System.Random
 import Reflex.Box2D.Foreign
 
 main :: IO ()
-main = setup >>= \w -> mainWidget (run w)
+main = mainWidget run
 
-setup :: IO WorldToken
-setup = do
-  w <- makeWorld (Vec2 0 10) True
-  floorFix <- makeFixture
-  fixtureDefSetDensity floorFix 1
-  fixtureDefSetFriction floorFix 0.5
-  fixtureDefSetRestitution floorFix 0.2
-  b <- makeBodyDef
-  bodyDefSetType b BodyTypeStatic
-  bodyDefSetX b 9
-  bodyDefSetY b 13
-  fixtureDefSetShape floorFix (ShapeBox 10 0.5)
-  worldCreateBodyAndFixture w b floorFix
-  return w
+------------------------------------------------------------------------------
+run :: SupportsReflexBox2D t m => m ()
+run = do
+  canv <- fst <$> elAttr'
+    ("id"     =: "canvas" <> "width"  =: "500"    <> "height" =: "500") blank
+  let canvEl = castToHTMLCanvasElement $ _element_raw canv
+  rec ff <- fixture def { fixtureDefConfig_initialShape = ShapeBox 10 0.5 }
+      f <- body w def { bodyDefConfig_initialFixtures = "floor" =: ff
+                      , bodyDefConfig_bodyType = BodyTypeStatic }
+      bf <- fixture def { fixtureDefConfig_initialShape = ShapeCircle 0.6 }
+      b <- body w def { bodyDefConfig_initialFixtures = "floor" =: ff
+                      , bodyDefConfig_bodyType = BodyTypeDynamic }
+      w <- world def { worldConfig_initialBodies = "f" =: f <> "b" =: b }
+  liftIO $ drawSetup ( drawSetup (worldToken w canvEl))
+  performEvent ()
+  return ()
 
-addSomething :: WorldToken -> IO BodyDefToken
-addSomething w = do
-  someFix <- makeFixture
-  bod <- makeBodyDef
-  [x,y,i,a,b] <- forM [0..4] $ \_ -> randomRIO (0,1)
-  let shape | i < 0.5   = ShapeBox    (a + 0.1) (b + 0.1)
-            | otherwise = ShapeCircle (a + 0.1)
-  fixtureDefSetShape someFix shape
-  fixtureDefSetDensity someFix 1
-  fixtureDefSetDensity someFix 0.5
-  fixtureDefSetDensity someFix 0.2
-  bodyDefSetType bod BodyTypeDynamic
-  bodyDefSetX bod (x*10)
-  bodyDefSetY bod (y*10)
-  worldCreateBodyAndFixture w bod someFix
-  return bod
 
-recenterSomething :: BodyDefToken -> IO ()
-recenterSomething t = do
-  print "Hi"
-  bodyDefSetX t 0
-  bodyDefSetY t 0
+-- main' :: IO ()
+-- main' = setup >>= \w -> mainWidget (run' w)
 
-run :: MonadWidget t m => WorldToken -> m ()
-run w = do
-  b <- button "Grav"
-  d <- button "Recenter"
-  canv <- fst <$> elAttr' "canvas" ("id" =: "canvas"
-                                   <> "width" =: "500"
-                                   <> "height" =:  "600") blank
-  pb <- getPostBuild
-  t0 <- liftIO getCurrentTime
-  ts <- tickLossy (1/60) t0
-  ps  <- liftIO newStdGen >>= \g -> poissonLossy g 0.1 t0
-  performEvent_ $ (liftIO (randomRIO (-10,10) >>= \g -> worldSetGravity w (Vec2 0 g))) <$ b
-  -- performEvent_ (liftIO (js_dirtyDrawSetup w) <$ pb)
-  performEvent_ (liftIO (drawSetup w (castToHTMLCanvasElement $ _element_raw canv)) <$ pb)
-  performEvent_ (liftIO (js_dirtyUpdate w) <$ ts)
-  performEvent_ (liftIO (addSomething w >> return ()) <$ ps)
-  c <- liftIO $ addSomething w
-  cs <- performEvent_ (liftIO (recenterSomething c) <$ d)
-  e <- fmap fst $ el' "div" $ text "Hello"
-  performEvent_ (liftIO (js_showWorld w) <$ domEvent Click e)
-  display . fmap (id :: Int -> Int) =<< count (domEvent Click e)
+-- setup :: IO WorldToken
+-- setup = do
+--   w <- makeWorld (Vec2 0 10) True
+--   floorFix <- makeFixture
+--   fixtureDefSetDensity floorFix 1
+--   fixtureDefSetFriction floorFix 0.5
+--   fixtureDefSetRestitution floorFix 0.2
+--   b <- makeBodyDef
+--   bodyDefSetType b BodyTypeStatic
+--   bodyDefSetX b 9
+--   bodyDefSetY b 13
+--   fixtureDefSetShape floorFix (ShapeBox 10 0.5)
+--   worldCreateBodyAndFixture w b floorFix
+--   return w
+
+-- addSomething :: WorldToken -> IO BodyDefToken
+-- addSomething w = do
+--   someFix <- makeFixture
+--   bod <- makeBodyDef
+--   [x,y,i,a,b] <- forM [0..4] $ \_ -> randomRIO (0,1)
+--   let shape | i < 0.5   = ShapeBox    (a + 0.1) (b + 0.1)
+--             | otherwise = ShapeCircle (a + 0.1)
+--   fixtureDefSetShape someFix shape
+--   fixtureDefSetDensity someFix 1
+--   fixtureDefSetDensity someFix 0.5
+--   fixtureDefSetDensity someFix 0.2
+--   bodyDefSetType bod BodyTypeDynamic
+--   bodyDefSetX bod (x*10)
+--   bodyDefSetY bod (y*10)
+--   worldCreateBodyAndFixture w bod someFix
+--   return bod
+
+-- recenterSomething :: BodyDefToken -> IO ()
+-- recenterSomething t = do
+--   print "Hi"
+--   bodyDefSetX t 0
+--   bodyDefSetY t 0
+
+-- run' :: MonadWidget t m => WorldToken -> m ()
+-- run' w = do
+--   b <- button "Grav"
+--   d <- button "Recenter"
+--   canv <- fst <$> elAttr' "canvas" ("id" =: "canvas"
+--                                    <> "width" =: "500"
+--                                    <> "height" =:  "600") blank
+--   pb <- getPostBuild
+--   t0 <- liftIO getCurrentTime
+--   ts <- tickLossy (1/60) t0
+--   ps  <- liftIO newStdGen >>= \g -> poissonLossy g 0.1 t0
+--   performEvent_ $ (liftIO (randomRIO (-10,10) >>= \g -> worldSetGravity w (Vec2 0 g))) <$ b
+--   -- performEvent_ (liftIO (js_dirtyDrawSetup w) <$ pb)
+--   performEvent_ (liftIO (drawSetup w (castToHTMLCanvasElement $ _element_raw canv)) <$ pb)
+--   performEvent_ (liftIO (js_dirtyUpdate w) <$ ts)
+--   performEvent_ (liftIO (addSomething w >> return ()) <$ ps)
+--   c <- liftIO $ addSomething w
+--   cs <- performEvent_ (liftIO (recenterSomething c) <$ d)
+--   e <- fmap fst $ el' "div" $ text "Hello"
+--   performEvent_ (liftIO (js_showWorld w) <$ domEvent Click e)
+--   display . fmap (id :: Int -> Int) =<< count (domEvent Click e)
--- src/Reflex/Box2D.hs
+++ src/Reflex/Box2D.hs
@@ -4,11 +4,17 @@
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE JavaScriptFFI     #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes        #-}
+{-# LANGUAGE RecursiveDo       #-}
 
 module Reflex.Box2D where
 
+import Control.Applicative (liftA2)
+import Control.Monad (liftM2)
 import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Fix (MonadFix)
 import Data.Default
+import Data.Time
 import Data.Map (Map)
 import qualified Data.Text as T
 import Reflex.Dom
@@ -17,27 +23,34 @@ import GHC.Generics
 import Reflex.Box2D.Types
 
 type SupportsReflexBox2D t m = (Reflex t, MonadIO m, PerformEvent t m,
-                                MonadIO (Performable m))
+                                MonadIO (Performable m), MonadFix m,
+                                PostBuild t m, TriggerEvent t m,
+                                MonadHold t m)
 
 
-world :: SupportsReflexBox2D t m => WorldConfig t -> m (World t)
-world (WorldConfig g0 dG s0 dS) = do
+world :: forall t m.SupportsReflexBox2D t m => WorldConfig t -> m (World t)
+world (WorldConfig g0 dG s0 dS bods0 dBods clockPhys clockDraw) = do
   w <- liftIO $ makeWorld g0 s0
+  let mkTicks ClockNever = return never
+      mkTicks (ClockRegularFrequency freq) = (() <$) <$> do
+        liftIO getCurrentTime >>= tickLossy (realToFrac freq)
+  (physicsTs, drawTs) <- liftM2 (,) (mkTicks clockPhys) (mkTicks clockDraw)
   performEvent_ (ffor dG $ liftIO . worldSetGravity w)
-  return $ World w 
+  return $ World w physicsTs drawTs
 
+-------------------------------------------------------------------------------
 body :: SupportsReflexBox2D t m => World t -> BodyDefConfig t -> m (BodyDef t)
 body w (BodyDefConfig bType p0 dp f0 df) = do
   b <- liftIO makeBodyDef
-  liftIO $ bodyDefSetType b bType >> bodyDefSetX b (x p0)
-                                  >> bodyDefSetY b (y p0)
-  pSim <- performEvent $ ffor (world_draw_ticks w) $ \() -> do
-    liftA2 Vec2 (bodyDefGetX b) (bodyDefGetY b)
-  p <- holdDyn p0 $ leftmost [dp ] -- TODO
-  fixtures0 <- liftIO $ ffor f0 $ \fixture -> do
-    worldCreateBodyAndFixture w b
-  fixtures <- holdDyn fixtures0 never -- TODO: add/delete fixtures
-  return $ BodyDef bType 
+  liftIO $ bodyDefSetType b bType
+
+  p <- setAndTrack p0 dp
+       (\(Vec2 x y) -> bodyDefSetX b x >> bodyDefSetY b y)
+       ((liftA2 Vec2 (bodyDefGetX b) (bodyDefGetY b)) <$ world_draw_ticks w)
+
+  fixtures <- foldDyn (applyMap) f0 df
+  let contacts = constDyn ()
+  return $ BodyDef bType p b fixtures contacts
 
 data WorldConfig t = WorldConfig {
     worldConfig_initialGravity   :: Vec2
@@ -47,12 +60,11 @@ data WorldConfig t = WorldConfig {
   , worldConfig_initialBodies    :: Map T.Text (BodyDef t)
   , worldConfig_setBodies        :: Event t (Map T.Text (Maybe (BodyDef t)))
   , worldConfig_physics_ticks    :: ClockSource
-  , worldConfig_draw_ticks             :: ClockSource
+  , worldConfig_draw_ticks       :: ClockSource
   }
 
 instance Reflex t => Default (WorldConfig t) where
-  def = WorldConfig (Vec2 0 10) never True never
-    (ClockRegularFrequency 600) (ClockRegularFrequency 60)
+  def = WorldConfig (Vec2 0 10) never True never mempty never (ClockRegularFrequency 600) (ClockRegularFrequency 60)
 
 data World t = World {
     world_token         :: WorldToken
@@ -61,20 +73,24 @@ data World t = World {
   }
 
 data FixtureDefConfig t = FixtureDefConfig
-  { fixtureDefConfig_initialDensity :: Double
-  , fixtureDefConfig_setDensity     :: Event t Double
-  , fixtureDefConfig_initialShape   :: Shape
-  , fixtureDefConfig_setShape       :: Event t Shape
+  { fixtureDefConfig_initialDensity     :: Double
+  , fixtureDefConfig_setDensity         :: Event t Double
+  , fixtureDefConfig_initialFriction    :: Double
+  , fixtureDefConfig_setFriction        :: Event t Double
+  , fixtureDefConfig_initialRestitution :: Double
+  , fixtureDefConfig_setRestitution     :: Event t Double
+  , fixtureDefConfig_initialShape       :: Shape
+  , fixtureDefConfig_setShape           :: Event t Shape
   }
 
 instance Reflex t => Default (FixtureDefConfig t) where
-  def = FixtureDefConfig 1 never (ShapeCircle 1) never
+  def = FixtureDefConfig 1 never 0.5 never 0.2 never (ShapeCircle 1) never
 
 data FixtureDef t = FixtureDef {
     fixtureDef_density     :: Dynamic t Double
   , fixtureDef_friction    :: Dynamic t Double
   , fixtureDef_restitution :: Dynamic t Double
-  , fixtureDef_shape       :: Shape
+  , fixtureDef_shape       :: Dynamic t Shape
   , fixtureDef_token       :: FixtureToken
   }
 
@@ -83,7 +99,7 @@ data BodyDefConfig t = BodyDefConfig
   , bodyDefConfig_initialPosition :: Vec2
   , bodyDefConfig_setPosition :: Event t Vec2
   , bodyDefConfig_initialFixtures :: Map T.Text (FixtureDef t)
-  , bodyDefConfig_setFixtures :: Map T.Text (Maybe (FixtureDef t))
+  , bodyDefConfig_setFixtures :: Event t (Map T.Text (Maybe (FixtureDef t)))
   }
 
 instance Reflex t => Default (BodyDefConfig t) where
@@ -97,3 +113,25 @@ data BodyDef t = BodyDef {
   , bodyDef_fixtures  :: Dynamic t (Map T.Text (FixtureDef t))
   , bodyDef_contacts  :: Dynamic t () -- TODO
   }
+
+setAndTrack
+  :: SupportsReflexBox2D t m
+  => a
+  -> Event t a
+  -> (a -> IO ())
+  -> Event t (IO a)
+  -> m (Dynamic t a)
+setAndTrack a0 da act probe = do
+  liftIO $ act a0
+  performEvent $ liftIO . act <$> da
+  simulationUpdates <- performEvent (liftIO <$> probe)
+  holdDyn a0 $ leftmost [da, simulationUpdates]
+
+fixture :: SupportsReflexBox2D t m => FixtureDefConfig t -> m (FixtureDef t)
+fixture (FixtureDefConfig d0 dd f0 df r0 dr s0 ds) = do
+  f <- liftIO makeFixture
+  density <- setAndTrack d0 dd (fixtureDefSetDensity f) never
+  friction <- setAndTrack f0 df (fixtureDefSetFriction f) never
+  shape <- setAndTrack s0 ds (fixtureDefSetShape f) never
+  restitution <- setAndTrack r0 dr (fixtureDefSetRestitution f) never
+  return $ FixtureDef density friction restitution shape f
